12b124a58c0d92ada0ffd516cb35f147c4de4857
KhaledGhe <ghedirikhaled18@gmail.com>
2020-11-25 16:25:19 +0100

    123

---
+++ Project/js/script123.js
@@ -0,0 +1,39 @@
+$(function () {
+	$("#navbarToggle").blur(function (event) {
+		var screenWidth = window.innerWidth;
+		if (screenWidth < 768) {
+			$("#collapsable-nav").collapse('hide');
+		}
+	});
+});
+(function (global) {
+var dc = {};
+var homeHtml = "snippets/home-snippet.html";
+
+var insertHtml = function (selector, html) {
+	var targetElem = document.querySelector(selector);
+	targetElem.innerHtml = html;
+};
+
+var showLoading = function (selector) {
+	var	html = "<div class='text-center'>";
+	html += "<img src='image/ajax-loader.gif'></div>";
+	insertHtml(selector, html);
+};
+
+document.addEventListener("DOMContentLoaded", function (event) {
+	
+	showLoading("#main-content");
+	$ajaxUtils.sendGetRequest(
+		homeHtml,
+		function (responseText) {
+		 	document.querySelector("#main-content")
+		 	.innerHtml = responseText;
+		 }, 
+        false);
+	console.log($ajaxUtils)
+});
+
+global.$dc = dc;
+
+})(window);
